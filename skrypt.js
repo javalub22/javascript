bc0ede4ae28bfa62efab5bf04401a54aa13cc755
lkre <lukasz.res@gmail.com>
2020-09-27 15:31:03 +0200

    validator formularza

--- skrypt.js
+++ skrypt.js
@@ -1,11 +1,34 @@
 function validateForm(formName) {
+    let isValid = true;
     let form = document.forms[formName];
     for (let formElement of form) {
         if (formElement.tagName.toLowerCase() === 'input') {
             if (formElement.value === '') {
-                alert(formElement.parentElement.innerText + ' musi zostać wypełnione');
-                return false;
+                changeStaleToWarning(formElement);
+                isValid = false;
+            } else {
+                formElement.style = "";
             }
         }
     }
+    if (!isValid) {
+        showWarning();
+    } else {
+        hideWarning();
+    }
+    return isValid;
+}
+
+function changeStaleToWarning(formElement) {
+    formElement.style = 'background-color:  #ffb3b3; border-color: red';
+}
+
+function showWarning() {
+    document.getElementById('warning')
+        .removeAttribute('hidden');
+}
+
+function hideWarning() {
+    document.getElementById('warning')
+        .setAttribute('hidden', '');
 }
