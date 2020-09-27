a859d808bd8bf539a0eae6ee1d7a4904ac23e57b
lkre <lukasz.res@gmail.com>
2020-09-27 13:46:59 +0200

    zadanie-javascript 15

--- skrypt.js
+++ skrypt.js
@@ -1,3 +1,8 @@
-function alertFunction(){
-    alert("Test");
+function swap() {
+    let barFoo = document.getElementById("foobar");
+    if (barFoo.innerHTML === "Foo bar!") {
+        barFoo.innerHTML = "Bar foo!";
+    } else {
+        barFoo.innerHTML = "Foo bar!";
+    }
 }
